fc2407bbfe87229a7a52877b73342df6932c447f
Eduardo <du_du.mba@hotmail.com>
2020-09-21 17:00:00 +0100

    commit

---
+++ src/database/migrations/1600703913335-CreateTransactions.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export default class CreateTransactions1600703913335 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+  }
+
+}
